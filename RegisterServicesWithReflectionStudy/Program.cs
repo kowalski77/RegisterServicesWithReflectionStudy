1513d080ad5a9784b64e0ce0c1ca8e7a879b03e7
Christian Aranda <christian.aranda@contractors.roche.com>
2021-07-10 18:30:03 +0200

    Add project files.

---
+++ RegisterServicesWithReflectionStudy/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RegisterServicesWithReflectionStudy
+{
+    internal static class Program
+    {
+        private static IServiceProvider serviceProvider;
+
+        private static void Main()
+        {
+            ConfigureServices();
+ 
+            // Do stuff...        
+ 
+            Console.ReadKey();
+ 
+            DisposeServices();
+        }
+
+
+        private static void ConfigureServices()
+        {
+            var services = new ServiceCollection(); 
+
+            serviceProvider = services.BuildServiceProvider(true);
+        }
+
+        private static void DisposeServices()
+        {
+            switch (serviceProvider)
+            {
+                case null:
+                    return;
+                case IDisposable disposable:
+                    disposable.Dispose();
+                    break;
+            }
+        }
+    }
+}
